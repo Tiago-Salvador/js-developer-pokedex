b6f7f60fb2a50c11374295e0c22df70e76c30a2e
Tiago-Salvador <141890716+Tiago-Salvador@users.noreply.github.com>
2024-06-17 23:33:38 -0300

    Update Servidor

--- assets/js/poke-api.js
+++ assets/js/poke-api.js
@@ -21,7 +21,7 @@ pokeapi.getPokemonDetail = (pokemon) => {
         .then(convertPokeApiDetailToPokemon)
 }
 
-pokeapi.getPokemons = (offset = 0, limit = 10) => {
+pokeapi.getPokemons = (offset = 0, limit = 50) => {
     const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
     return fetch(url)
         .then((response) => response.json())
